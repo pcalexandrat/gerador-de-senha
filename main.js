c9964fc24c9f6370dbce7c3b80ac9c33352dc3aa
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-06-05 15:28:33 -0300

    num

---
+++ main.js
@@ -0,0 +1,14 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes);
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
