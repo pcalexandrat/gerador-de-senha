b785ec254045c4bf1279273fe421613ae830b662
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2025-06-05 18:06:21 -0300

    .

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function aumentaTamanho() {
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 
-for (i=0; i < checkbox.length;i++){
+for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
 
@@ -36,6 +36,7 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
 const simbolos = '!@%*?';
+
 geraSenha();
 
 function geraSenha() {
@@ -61,7 +62,4 @@ function geraSenha() {
     }
     campoSenha.value = senha;
 }
-
-
-
 
